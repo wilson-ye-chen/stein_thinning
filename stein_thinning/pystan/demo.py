14263b20e308b6a7baf4bb6130ecea5b5c8835a9
Wilson Ye Chen <ye.chen@uts.edu.au>
2020-05-04 11:00:47 +1000

    Added a PyStan example.

---
+++ stein_thinning/pystan/demo.py
@@ -0,0 +1,27 @@
+"""Post-process output from Stan."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pystan import StanModel
+from stein_thinning.thinning import thin
+
+# Simple bivariate Gaussian model
+mc = """
+parameters {vector[2] x;}
+model {x ~ multi_normal([0, 0], [[1, 0.8], [0.8, 1]]);}
+"""
+sm = StanModel(model_code=mc)
+fit = sm.sampling(iter=1000)
+
+# Extract sampled points and gradients
+smp = fit['x']
+scr = np.apply_along_axis(fit.grad_log_prob, 1, smp)
+
+# Obtain a subset of 40 points
+x, s = thin(smp, scr, 40)
+
+# Plot point-set over trace
+plt.figure()
+plt.plot(smp[:,0], smp[:,1], color=(0.4, 0.4, 0.4), linewidth=1)
+plt.plot(x[:,0], x[:,1], 'r.', markersize=10)
+plt.show()
