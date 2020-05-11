8864afe6e6f64979f04b1652c565e676844b0edf
Wilson Ye Chen <ye.chen@uts.edu.au>
2020-05-11 22:38:52 +1000

    Started fixing memory issue.

--- stein_thinning/thinning.py
+++ stein_thinning/thinning.py
@@ -1,20 +1,14 @@
 """Core functions of Stein Thinning."""
 
 import numpy as np
-from stein_thinning.stein import greedy, fmin_grid
 from stein_thinning.kernel import make_imq
 
-def thin(smp, scr, n, pre='sclmed'):
+def thin(smp, scr, m, pre='sclmed'):
     # Sample dimension
-    dm = smp.shape[1]
+    n = smp.shape[0]
 
-    # Eliminate duplicates
-    smp_uni, idx_uni = np.unique(smp, return_index=True, axis=0)
-    scr_uni = scr[idx_uni]
-
-    # Run SP using grid search
-    vfs = lambda x: scr_uni
+    # Vectorised Stein kernel
     vfk0 = make_imq(smp, scr, pre)
-    fmin = lambda vf, x, vfs: fmin_grid(vf, x, vfs, smp_uni)
-    x, s, _ = greedy(dm, vfs, vfk0, fmin, n)
+
+    k0 = np.empty((n, m))
     return x, s
