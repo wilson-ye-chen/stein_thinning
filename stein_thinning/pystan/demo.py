21aad46664142a4582d99cbc91f020b1858b884d
Wilson Ye Chen <ye.chen@uts.edu.au>
2020-05-20 00:42:35 +1000

    minor style issues.

--- stein_thinning/pystan/demo.py
+++ stein_thinning/pystan/demo.py
@@ -18,10 +18,10 @@ smp = fit['x']
 scr = np.apply_along_axis(fit.grad_log_prob, 1, smp)
 
 # Obtain a subset of 40 points
-iSel = thin(smp, scr, 40)
+idx = thin(smp, scr, 40)
 
 # Plot point-set over trace
 plt.figure()
 plt.plot(smp[:,0], smp[:,1], color=(0.4, 0.4, 0.4), linewidth=1)
-plt.plot(smp[iSel, 0], smp[iSel, 1], 'r.', markersize=10)
+plt.plot(smp[idx, 0], smp[idx, 1], 'r.', markersize=10)
 plt.show()
