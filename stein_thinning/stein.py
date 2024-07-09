a79d108e9b3c0811d82c36680e93150365d7df8c
Artem Glebov <artem.glebov@gmail.com>
2024-07-09 19:51:18 +0100

    Remove unused functions from module stein

--- stein_thinning/stein.py
+++ stein_thinning/stein.py
@@ -4,26 +4,6 @@ from typing import Callable
 
 import numpy as np
 
-def fmin_grid(vf, x, vfs, grid):
-    s = vfs(grid)
-    val = vf(grid, s)
-    i = np.argmin(val)
-    return grid[i], s[i], grid.shape[0]
-
-
-def vfps(x_new, s_new, x, s, i, vfk0):
-    k0aa = vfk0(x_new, x_new, s_new, s_new)
-    if i > 0:
-        n_new = x_new.shape[0]
-        a = np.tile(x_new, (i, 1))
-        b = np.repeat(x[0:i], n_new, 0)
-        sa = np.tile(s_new, (i, 1))
-        sb = np.repeat(s[0:i], n_new, 0)
-        k0ab = np.reshape(vfk0(a, b, sa, sb), (-1, n_new))
-        return np.sum(k0ab, axis=0) * 2 + k0aa
-    else:
-        return k0aa
-
 
 def kmat(
         sample: np.ndarray,
@@ -87,14 +67,3 @@ def ksd(
     ind1, ind2 = np.tril_indices(n, -1)  # indices of elements below diagonal
     ks = np.cumsum(np.diag(km)) + 2 * np.concatenate([[0], np.cumsum(km[ind1, ind2])[np.cumsum(np.arange(1, n)) - 1]])
     return np.sqrt(ks) / np.arange(1, n + 1)
-
-
-def greedy(d, vfs, vfk0, fmin, n):
-    x = np.empty((n, d))
-    s = np.empty((n, d))
-    e = np.empty(n)
-    for i in range(n):
-        vf = lambda x_new, s_new: vfps(x_new, s_new, x, s, i, vfk0)
-        x[i], s[i], e[i] = fmin(vf, x, vfs)
-        print(f'i = {i}')
-    return x, s, e
